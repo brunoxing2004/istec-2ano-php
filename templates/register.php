71a6eea79147504eaa549874b6a6e1e031ac8804
brunoxing2004 <mail@brunosilva.co.uk>
2024-01-07 16:10:52 +0000

    Update register.php

--- templates/register.php
+++ templates/register.php
@@ -20,6 +20,19 @@
             <a href="login.php">Login</a>
         </div>
         </header>
+
+        <nav id="menu">
+            <input type="checkbox" id="hamburger"> 
+            <label class="hamburger" for="hamburger"></label>
+            <ul>
+                <li><a href="index.php">Local</a></li>
+                <li><a href="index.php">World</a></li>
+                <li><a href="index.php">Politics</a></li>
+                <li><a href="index.php">Sports</a></li>
+                <li><a href="index.php">Science</a></li>
+                <li><a href="index.php">Weather</a></li>
+            </ul>
+        </nav>
     </body>
 <?php } ?>
 
